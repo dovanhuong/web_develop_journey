7b9b98c75e5ee99efd1064f11de88e30f91848ca
Tony Do <dvhbkhn@gmail.com>
2024-03-30 00:06:15 +0900

    query selector root

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,2 +1,17 @@
+// Creator: Tony Do
+// Date: 2024.03.29
+// Email: dvhbkhn@gmail.com
+// Revision 1.0
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const App = () => { 
+    return (
+        <div>Hi there!</div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
